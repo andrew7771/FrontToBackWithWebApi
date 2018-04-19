a22426f1b51ce72d0034518ca804d06cb1bf146a
aturianskiy <aturianskiy@ATURIANSKIY>
2018-04-19 15:04:33 +0300

    Client uses Web API Service to save data to storage

--- APM.Client/common/productResource.js
+++ APM.Client/common/productResource.js
@@ -1,10 +1,13 @@
-﻿(function() {
+﻿(function () {
     "use strict";
 
     angular.module("common.services").factory("productResource",
         ["$resource", "appSettings", productResource]);
 
     function productResource($resource, appSettings) {
-        return $resource(appSettings.serverPath + "/api/products/:id");
+        return $resource(appSettings.serverPath + "/api/products/:id", null,
+            {
+                'update': { method: 'PUT' }
+            });
     }
 })();
--- APM.WebAPI/APM.WebAPI/App_Start/WebApiConfig.cs
+++ APM.WebAPI/APM.WebAPI/App_Start/WebApiConfig.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Http.OData.Extensions;
-
-using APM.WebAPI.Models;
-using Microsoft.OData.Edm;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
